bb625c94559c541891f796c0d51af34d005b9f47
SellerOfDigitalBalance <faiz.muhammad.rijal.fikri@gmail.com>
2025-11-02 09:10:07 +0700

    again2

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -23,7 +23,7 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'nik' => $this->faker->numerify('################'),
+            'nik' => fake()->numerify('################'),
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
