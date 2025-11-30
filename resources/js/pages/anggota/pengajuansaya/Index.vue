ad5990de0181b52f0c35146d382cd9c03392323f
SellerOfDigitalBalance <faiz.muhammad.rijal.fikri@gmail.com>
2025-11-30 22:48:20 +0700

    filter data pada menu yang ada di anggota

--- resources/js/pages/anggota/pengajuansaya/Index.vue
+++ resources/js/pages/anggota/pengajuansaya/Index.vue
@@ -185,7 +185,7 @@ const handleBatalkanPengajuan = (id: number) => {
                                         class="w-full rounded-md border border-border bg-background px-3 py-2 text-sm text-foreground shadow-sm focus:ring-2 focus:ring-primary focus:outline-none"
                                     >
                                         <option value="">
-                                            -- Semua category --
+                                            -- Semua Status --
                                         </option>
                                         <option value="dipinjam">
                                             Dipinjam
